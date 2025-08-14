1cb82be679e7a381a9d0da751a47d4608ebefc9b
ΑΝΔΡΕΑΣ ΚΟΓΙΑΣ <kogias22@gmail.com>
2025-08-14 16:47:44 +0300

    section 3

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using Microsoft.CodeAnalysis.Options;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,10 +15,13 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-  
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();

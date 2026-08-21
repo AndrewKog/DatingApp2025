432a6401a6f694325b92cff20e81738ed120618e
ΑΝΔΡΕΑΣ ΚΟΓΙΑΣ <kogias22@gmail.com>
2026-08-21 12:48:48 +0300

    end of section 9

--- DatingApp/API/Extensions/AppUserExtensions.cs
+++ DatingApp/API/Extensions/AppUserExtensions.cs
@@ -14,6 +14,7 @@ public static class AppUserExtensions
             Id = user.Id,
             Displayname = user.Displayname,
             Email = user.Email,
+            ImageUrl=user.ImageUrl,
             Token = tokenService.CreateToken(user)
 
         };
---
+++ DatingApp/client/src/features/members/member-messsages/member-messsages.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-messsages',
+  imports: [],
+  templateUrl: './member-messsages.html',
+  styleUrl: './member-messsages.css'
+})
+export class MemberMesssages {
+
+}
